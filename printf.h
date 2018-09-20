b2ca5ee8cf93275eab4d338e1c8dddce12696397
John Kelleher <jkellehe@e1z2r3p13.42.us.org>
2018-09-20 12:21:17 -0700

    yeah

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: jkellehe <jkellehe@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/23 16:24:30 by jkellehe          #+#    #+#             */
-/*   Updated: 2018/09/19 17:45:06 by jkellehe         ###   ########.fr       */
+/*   Updated: 2018/09/20 11:44:53 by jkellehe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # define ExOr(x, y) ((x && !y) || (!x && y))
 # define INPUTS(x) (IS_TYPE(x) || isDIGIT(x) || isID(x) || isFLAG(x))
 # define IS_UPPER(x) (x >= 'A' && x <= 'Z')
-# define IS_LOWER(x) (x >= 'a' && x <= 'z')
+# define IS_LOW(x) (x >= 'a' && x <= 'z')
 # define IS_TYPE(x) (x == 's' || x == 'S' || x == 'p' || x == 'd' || x == 'D' || x == 'i' || x == 'o' || x == 'O' || x == 'u' || x == 'U' || x == 'x' || x == 'X' || x == 'c' || x == 'C' || x == 'b' || x == 'f' || x == 'F' || x == 'a' || x == 'A' || x == '%')
 # define NUMBERS(x) (*x == 'd' || *x == 'D' || *x == 'x' || *x == 'X' || *x == 'b' || *x == 'o' || *x == 'O' || *x == 'u')
 # define FLOATS(x) (*x == 'f' || *x == 'F' || *x =='a' || *x == 'A')
@@ -33,11 +33,11 @@
 # define LL(x) (x[-1] == 'l' && x[-2] == 'l')
 # define is_unsign(x) (*x == 'x' || *x == 'X' || *x == 'o' || *x == 'O' || *x == 'u')
 # define isFLAG(x) (x == '#' || x == '-' || x == '+')
-# define Ox(tree) (hash(tree) && !tree->zero && (tree->car == 'x' || tree->car == 'X'))
-# define O(tree) (tree->car == 'o' || tree->car == 'O')
-# define X(tree) (tree->car == 'x' || tree->car == 'X')
+# define Ox(tree) (hash(tree) && !tree->zero && (tree->c[0] == 'x' || tree->c[0] == 'X'))
+# define O(tree) (tree->c[0] == 'o' || tree->c[0] == 'O')
+# define X(tree) (tree->c[0] == 'x' || tree->c[0] == 'X')
 # define hash(tree) (tree->hash)
-# define SingleSpace(x) (x->car == 'd' && x->space && !x->neg && !x->left && !x->O && !x->X && !x->percent && !x->l && !x->ll && !x->decimal && !x->hash && !x->zero && !x->z_pad && !x->dot && !x->plus && (x->prec == 10000) && (x->width <= 0))
+# define SingleSpace(x) (x->c[0] == 'd' && x->space && !x->neg && !x->left && !x->O && !x->X && !x->percent && !x->l && !x->ll && !x->decimal && !x->hash && !x->zero && !x->z_pad && !x->dot && !x->plus && (x->prec == 10000) && (x->width <= 0))
 
 
 typedef struct s_ap t_ap;
@@ -62,6 +62,7 @@ struct					s_ap
 	uint8_t plus;
 	uint8_t neg;
 	uint8_t space;
+	uint8_t set_ret;
 	char *c;
 	char car;
 
@@ -74,7 +75,7 @@ void					ft_put_wstr_fd_prec(wchar_t *s, int fd, int prec, t_ap *tree);
 char					*ft_spad(char *s, int prec, t_ap *tree);
 char					*ft_umaxtoa_base(uintmax_t n, uintmax_t base, char *format);
 int						bt_putstr_fd(char const *s, int fd, t_ap *tree);
-int						floot(va_list ap, char *format, t_ap *tree);
+void					floot(va_list ap, char *format, t_ap *tree);
 char					*ft_ftoa_base(double n, long long base, char *format);
 void					ft_putstr_fd_prec(char *s, int fd, int prec, t_ap *tree);
 int						precision(char *format, va_list ap, t_ap *tree);
@@ -83,7 +84,7 @@ int						ass_f(void (**p) (va_list ap, char *format, t_ap *tree), t_ap *tree);
 void						digit(va_list ap, char *format, t_ap *tree);
 int						ft_printf(const char * restrict format, ...);
 void					flags(char *c, t_ap *tree);
-int						decimals(double holder, float base, t_ap *tree);
+intmax_t						decimals(double holder, float base, t_ap *tree);
 
 
 # endif
