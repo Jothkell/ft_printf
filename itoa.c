8dee24c354b25f95bf18c8bb5af8be1a43f6d822
John Kelleher <jkellehe@e1z3r7p13.42.us.org>
2018-11-29 12:58:06 -0800

    j

--- itoa.c
+++ itoa.c
@@ -6,7 +6,7 @@
 /*   By: jkellehe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 19:36:01 by jkellehe          #+#    #+#             */
-/*   Updated: 2018/11/29 12:55:17 by jkellehe         ###   ########.fr       */
+/*   Updated: 2018/11/29 12:57:06 by jkellehe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,7 +114,7 @@ void			floot(va_list ap, char *format, t_ap *tree)
 	ft_putstr_fd_prec(ft_maxtoa_base((intmax_t)holder,
 									(intmax_t)base, format), tree);
 	tree->prec = prec;
-    tree->prec = (!tree->prec && !tree->dot) ? (6) : (tree->prec);
+    tree->prec = ((!tree->prec || tree->prec == 10000) && !tree->dot) ? (6) : (tree->prec);
     tree->ret += (tree->prec) ? (write(1, ".", 1)) : (0);
 	ft_putstr_fd_prec(ft_maxtoa_base((intmax_t)temp,
 									(intmax_t)base, format), tree);
